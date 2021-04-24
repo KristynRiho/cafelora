5a09db04210e9af5c9bb4f8a7d054d5c9e8d8650
Kristýna Říhová <rihokristyn@gmail.com>
2021-04-24 11:47:56 +0200

    tretí ukol - hotov

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './style.css';
 
-console.log('funguju!');
+//schování navigace a zobrazení
+const navBtnElm = document.querySelector('#nav-btn');
+navBtnElm.addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+});
+
+//vybraní vsech polozek navigace
+const allNavItems = document.querySelectorAll('.nav__item');
+for (let i = 0; i < allNavItems.length; i++) {
+  allNavItems[i].addEventListener('click', () => {
+    document.querySelector('nav').classList.add('nav-closed');
+  });
+}
