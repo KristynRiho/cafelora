e1e0609e2d3500f994066e52a7bcf47bd0ad4a9c
Kristýna Říhová <rihokristyn@gmail.com>
2021-04-24 14:51:25 +0200

    5.část - hotovo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style.css';
 
+import { Layer } from './Layer/index.js';
+
 //schování navigace a zobrazení
 const navBtnElm = document.querySelector('#nav-btn');
 navBtnElm.addEventListener('click', () => {
@@ -29,3 +31,64 @@ orderBtnElm.addEventListener('click', () => {
     ordered = false;
   }
 });
+
+//ingredience jako komponenty
+const data = {
+  color: '#feeeca',
+  label: 'mléčná pěnička',
+};
+
+const layerContainerElm = document.querySelector('.layer-container');
+layerContainerElm.innerHTML = Layer(data);
+layerContainerElm.innerHTML += Layer({
+  color: '#fed7b0',
+  label: 'teplé mléko',
+});
+layerContainerElm.innerHTML += Layer({
+  color: '#613916',
+  label: 'espresso',
+});
+
+//seznam ingrediencí
+// const layers = [
+//   {
+//     color: '#feeeca',
+//     label: 'mléčná pěna',
+//   },
+//   {
+//     color: '#fed7b0',
+//     label: 'teplé mléko',
+//   },
+//   {
+//     color: '#613916',
+//     label: 'espresso',
+//   },
+// ];
+
+// layers.forEach((item) => {
+//   layerContainerElm.innerHTML += Layer(item)
+// })
+
+//Nápoj jako komponenta
+// const Drink = (props) => {
+//   const root = document.createElement('div');
+//   root.classList.add('layer-container');
+
+//   return root;
+// };
+
+// const data2 = {
+//   id: 'romano',
+//   name: 'Romano',
+//   ordered: false,
+//   layers: [
+//     {
+//       color: '#fbdf5b',
+//       label: 'citrón',
+//     },
+//     {
+//       color: '#613916',
+//       label: 'espresso',
+//     },
+//   ],
+// };
